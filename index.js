c048186868214e037f7c5711afacacecc33b7913
Joseph Perla <@notanemail@jperla>
2016-05-07 11:02:35 -0700

    socket io

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
   res.send('<h1>Hello world</h1>');
@@ -7,6 +8,9 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 var port = process.env.PORT || 3000;
