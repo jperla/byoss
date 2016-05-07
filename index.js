0c672ead3d69ce9ca7104d8f23d58d4b20d356fb
Joseph Perla <@notanemail@jperla>
2016-05-07 11:01:16 -0700

    monitor connections

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.send('<h1>Hello world</h1>');
 });
 
+io.on('connection', function(socket) {
+  console.log('a user connected');
+});
+
 var port = process.env.PORT || 3000;
-http.listen(port, function(){
+http.listen(port, function() {
   console.log('listening on *:' + port);
 });
